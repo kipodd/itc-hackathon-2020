96c4fed1589acca201c24ff1c06787dd3f67ddaf
Itzik Pazuelo <itpazu@gmail.com>
2020-06-09 18:41:31 +0300

    before first post

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import AddNewProject from './components/addProjectForm';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <div className='App'>
+        <AddNewProject
+        // submitForm={(form) => {
+        //   setFormResults(form);
+        // }}
+        ></AddNewProject>
+      </div>
     </div>
   );
 }
