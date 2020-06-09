dad6fc4719a819aba1497b9feb6310c94d6f23a6
kipodd <17299316+kipodd@users.noreply.github.com>
2020-06-09 12:50:51 +0300

    Added create project route

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 from bson import json_util
-from flask import Flask
+from flask import Flask, request
 
 from projects_data_layer import ProjectsDataLayer
 
@@ -16,6 +16,12 @@ def get_all_projects():
         {"Content-Type": "application/json", "Access-Control-Allow-Origin": "*"},
     )
 
+
+@app.route("/projects", methods=["POST"])
+def set_project():
+    print(1)
+    new_project = projects_collection.set_project(request.json)
+    return new_project.to_json(), 201, {"Content-Type": "application/json"}
 
 if __name__ == "__main__":
     app.run()
