4bdec15c37ad2bca73cc191a40d756a3d5b72a34
selimmizrahi <mizrahiselim@gmail.com>
2020-06-10 15:23:24 +0300

    nologinnosignup

--- client/src/components/FeedPage.jsx
+++ client/src/components/FeedPage.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import axios from "axios";
-import {
-  BrowserRouter as Router,
-  
-  withRouter,
-} from "react-router-dom";
+import { BrowserRouter as Router, withRouter } from "react-router-dom";
 import volunteer from "./volunteer.jpg";
 
 class FeedPage extends React.Component {
@@ -32,7 +28,7 @@ class FeedPage extends React.Component {
     return (
       <div>
         {this.state.data && (
-          <div className='list_students'>
+          <div className="list_students">
             {this.state.data.map((user) => {
               return (
                 // <div class="card student">
@@ -46,14 +42,24 @@ class FeedPage extends React.Component {
                 <div class="card student mb-4" style={{ width: "69rem" }}>
                   <div class="card-body row">
                     <div className="col-9">
-                      <p class="card-text"> Name Of The Project: {user.name}</p>
-                      <p> Location: {user.city} </p>
+                      <p class="card-text">
+                        {" "}
+                        <b>Name Of The Project:</b> {user.name}
+                      </p>
                       <p>
-                        Projects starts at:{" "}
+                        {" "}
+                        <b>Description:</b> {user.description}{" "}
+                      </p>
+                      <p>
+                        {" "}
+                        <b>Location:</b> {user.city}{" "}
+                      </p>
+                      <p>
+                        <b>Projects starts at:</b>{" "}
                         {new Date(user.start_time.$date).toString()}
                       </p>
                       <p>
-                        Projects ends at:{" "}
+                        <b> Projects ends at:</b>{" "}
                         {new Date(user.end_time.$date).toString()}
                       </p>
                     </div>
--- client/src/components/SearchField.jsx
+++ client/src/components/SearchField.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import '../App.css';
-import { SearchNewProject } from '../lib/api';
-import Button from '@material-ui/core/Button';
-import DateFnsUtils from '@date-io/date-fns';
-import 'date-fns';
+import React, { useState } from "react";
+import "../App.css";
+import { SearchNewProject } from "../lib/api";
+import Button from "@material-ui/core/Button";
+import DateFnsUtils from "@date-io/date-fns";
+import "date-fns";
 
 const SeacrhBar = () => {
   const [searchInput, setSearchInput] = useState(null);
@@ -27,26 +27,26 @@ const SeacrhBar = () => {
   return (
     <>
       <form>
-        <div className='mt-5 d-flex justify-content-center'>
-          <h3>Volunt-here!</h3>
+        <div className="mt-5 d-flex justify-content-center">
+          <h3>VoluntHere!</h3>
         </div>
-        <div className='input-group mb-3 mt-1 d-flex justify-content-center '>
+        <div className="input-group mb-3 mt-1 d-flex justify-content-center ">
           <input
-            type='text'
-            className='search-focus '
-            placeholder='Seacrh for projects'
+            type="text"
+            className="search-focus "
+            placeholder="Seacrh for projects"
             aria-label="Recipient's username"
-            aria-describedby='button-addon2'
+            aria-describedby="button-addon2"
             onChange={(e) => {
               storeInput(e);
             }}
           />
         </div>
-        <div className='d-flex justify-content-center'>
+        <div className="d-flex justify-content-center">
           <Button
-            variant='contained'
-            color='primary'
-            type='button'
+            variant="contained"
+            color="primary"
+            type="button"
             onClick={(e) => {
               submitSearch(e);
             }}
